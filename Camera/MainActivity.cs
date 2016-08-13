81441aa072370c87f59920bbd1008fce52653c60
関口 恵太 <koron0902@gmail.com>
2016-08-13 22:21:10 +0900

    *機能改善・追加 　*シャッター後の処理のほとんどをBGに 　*GPS機能を追加 　*Exif編集(失敗) 　*プレビュー画面のサイズを修正

--- Camera/MainActivity.cs
+++ Camera/MainActivity.cs
@@ -8,6 +8,8 @@ using Android.Media;
 using Android.OS;
 using Android.Views;
 using Android.Widget;
+using Android.Locations;
+using Android.Content;
 
 using AndroidCamera = Android.Hardware.Camera;
 using Console = System.Console;
@@ -22,26 +24,37 @@ namespace Camera {
 		HardwareAccelerated = true
 	)]
 	[Obsolete]
-	public class MainActivity : Activity, TextureView.ISurfaceTextureListener, AndroidCamera.IPictureCallback, AndroidCamera.IAutoFocusCallback {
+	public class MainActivity : Activity,ILocationListener, TextureView.ISurfaceTextureListener, AndroidCamera.IPictureCallback, AndroidCamera.IAutoFocusCallback,AndroidCamera.IAutoFocusMoveCallback {
+
+		const uint ButtonWidth = 200;
+		const uint ButtonHeight = 150;
 
 		public AndroidCamera m_Camera;
 		TextureView m_TextureView;
 		bool isTakeEnabled = false;
+		bool isGPSEnabled = false;
 		Display disp;
 		int DisplayWidth = 1080;
 		int DisplayHeight = 1920;
+		Location m_Location;
+		LocationManager m_LocationManager;
+		string Latitude = "";
+		string Longitude = "";
+		int NowOriantation = 0;
 
 		float StartX = 0;
 		float StartY = 0;
 
 		protected override void OnCreate(Bundle savedInstanceState) {
 			base.OnCreate(savedInstanceState);
-			disp = WindowManager.DefaultDisplay;
-
-			GetDisplaySize();
 
 			Window.AddFlags(WindowManagerFlags.Fullscreen);	// 全画面表示に
-			RequestWindowFeature(WindowFeatures.NoTitle);	// タイトルバーをなくす
+			RequestWindowFeature(WindowFeatures.NoTitle);   // タイトルバーをなくす
+
+
+			disp = WindowManager.DefaultDisplay;
+			GetDisplaySize();
+
 
 			FrameLayout rootView = new FrameLayout(ApplicationContext);
 			SetContentView(rootView);
@@ -85,11 +98,21 @@ namespace Camera {
 
 			rootView.AddView(m_TextureView);
 
-
 			Button button = new Button(ApplicationContext);
 			button.Text = "撮影";
 			button.LayoutParameters = new ViewGroup.LayoutParams(200, 150);
 			button.Clickable = true;
+			button.SetWidth(200);
+			button.SetHeight(150);
+
+
+			if (!isLandScapeMode()) {
+				button.SetX((DisplayWidth / 2) -  100);
+				button.SetY(DisplayHeight - 300);
+			}else{
+				button.SetX((DisplayHeight / 2) - 100);
+				button.SetY(DisplayWidth - 150);
+			}
 
 			button.Touch += (sender, e) => {
 				if (e.Event.Action == MotionEventActions.Down){
@@ -99,28 +122,110 @@ namespace Camera {
 
 				if((!isTakeEnabled) && (e.Event.Action == MotionEventActions.Up)) {
 					// 撮影中ではない & ボタンから指が離れた
-					if ((e.Event.GetX() > button.GetX()) &&
-					    (e.Event.GetX() < (button.GetX() + button.Width)) &&
-					   (e.Event.GetY() > button.GetY()) &&
-					    (e.Event.GetY() < (button.GetY() + button.Height))) {
+					if ((e.Event.RawX > button.GetX()) &&
+					    (e.Event.RawX < (button.GetX() + ButtonWidth)) &&
+					   (e.Event.RawY > button.GetY()) &&
+					    (e.Event.RawY < (button.GetY() + ButtonHeight))) {
 						// 指が離れた時にボタン上に指があった
 						isTakeEnabled = true;
 						m_Camera.AutoFocus(this);
 					}
+
+					Toast.MakeText(ApplicationContext,
+								   "ButtonX:" + button.GetX() +
+								   "\nButtonY:" + button.GetY() +
+					               "\nGetX:" + e.Event.RawX +
+					               "\nGetY:" + e.Event.RawY,
+								   ToastLength.Short).Show();
 				}
 			};
 
 			rootView.AddView(button);
+
+			m_LocationManager = (LocationManager)GetSystemService(Context.LocationService);
+
 		}
 
-		public void OnSurfaceTextureAvailable(Android.Graphics.SurfaceTexture surface, int w, int h) {
+		protected override void OnResume() {
+			NowOriantation = DetectScreenOrientation();
+
 			m_Camera = AndroidCamera.Open();
+			m_Camera.SetAutoFocusMoveCallback(this);
+
+			// 写真のサイズを画面サイズの4倍(縦横2倍)に設定
+			var param = m_Camera.GetParameters();
+			param.SetPictureSize(DisplayHeight * 2, DisplayWidth * 2);
+			m_Camera.SetParameters(param);
+
+			m_LocationManager.RequestLocationUpdates(LocationManager.GpsProvider,
+													1,
+													0,
+													 this);
+			base.OnResume();
+
+		}
+
+		protected override void OnPause() {
+			m_LocationManager.RemoveUpdates(this);
+
+			m_Camera.StopPreview();
+			m_Camera.Release();
+			base.OnPause();
+		}
+
+		#region GPSリスナー
+		public void OnProviderEnabled(string provider) {
+
+		}
+
+		public void OnProviderDisabled(string provider) {
+
+		}
+
+		public void OnStatusChanged(string provider, Availability available, Bundle bundle){
+			switch(available){
+				case Availability.Available:
+					break;
+				case Availability.OutOfService:
+					break;
+				case Availability.TemporarilyUnavailable:
+					break;
+			}
+		}
+
+		public void OnLocationChanged(Location location){
+			m_Location = location;
+			Console.WriteLine("Latitude" + location.Latitude);
+			Console.WriteLine("Longitude" + location.Longitude);
+			Console.WriteLine("Accuracy" + location.Accuracy);
+			Console.WriteLine("Altitude" + location.Altitude);
+			Console.WriteLine("Time" + location.Time);
+			Console.WriteLine("Speed" + location.Speed);
+			Console.WriteLine("Bering" + location.Bearing);
+
+
+			int degree = (int)(location.Latitude);
+			int minute = (int)((location.Latitude - degree) * 60);
+			int second = (int)((((location.Latitude - degree) * 60) - minute) * 60000);
+			Latitude = degree + "/," + minute + "/1," + second + "/1000";
+
+
+			degree = (int)(location.Latitude);
+			minute = (int)((location.Latitude - degree) * 60);
+			second = (int)((((location.Latitude - degree) * 60) - minute) * 60000);
+			Longitude = degree + "/," + minute + "/1," + second + "/1000";
+		}
+
+		#endregion
+
+		#region SurfaceViewのリスナー
+		public void OnSurfaceTextureAvailable(Android.Graphics.SurfaceTexture surface, int w, int h) {
 
 			bool landscape = isLandScapeMode();
 			if(!landscape)
-				m_TextureView.LayoutParameters = new FrameLayout.LayoutParams(DisplayWidth * 2, DisplayHeight * 2);
+				m_TextureView.LayoutParameters = new FrameLayout.LayoutParams(DisplayWidth, DisplayHeight);
 			else
-				m_TextureView.LayoutParameters = new FrameLayout.LayoutParams(DisplayHeight * 2, DisplayWidth * 2);
+				m_TextureView.LayoutParameters = new FrameLayout.LayoutParams(DisplayHeight, DisplayWidth);
 
 			SetScreenOrientation();
 
@@ -133,29 +238,24 @@ namespace Camera {
 		}
 
 		public bool OnSurfaceTextureDestroyed(Android.Graphics.SurfaceTexture surface) {
-			m_Camera.StopPreview();
-			m_Camera.Release();
-
 			return true;
 		}
 
 		// SurfaceViewが更新された時に呼び出される
 		public void OnSurfaceTextureUpdated(Android.Graphics.SurfaceTexture surface) {
+			if (NowOriantation == DetectScreenOrientation())
+				return;
+
+			NowOriantation = DetectScreenOrientation();
 			SetScreenOrientation();
-
-			// 写真のサイズを画面サイズの4倍(縦横2倍)に設定
-			var param = m_Camera.GetParameters();
-			param.SetPictureSize(DisplayHeight * 2 , DisplayWidth * 2);
-			m_Camera.SetParameters(param);
-
 		}
 
 		public void OnSurfaceTextureSizeChanged(Android.Graphics.SurfaceTexture surface, int w, int h) {
 			bool landscape = isLandScapeMode();
 			if (!landscape)
-				m_TextureView.LayoutParameters = new FrameLayout.LayoutParams(DisplayWidth * 2, DisplayHeight * 2);
+				m_TextureView.LayoutParameters = new FrameLayout.LayoutParams(DisplayWidth, DisplayHeight);
 			else
-				m_TextureView.LayoutParameters = new FrameLayout.LayoutParams(DisplayHeight * 2, DisplayWidth * 2);
+				m_TextureView.LayoutParameters = new FrameLayout.LayoutParams(DisplayHeight, DisplayWidth);
 
 			SetScreenOrientation();
 
@@ -167,7 +267,9 @@ namespace Camera {
 				System.Console.WriteLine(e.Message);
 			}
 		}
+		#endregion
 
+		#region カメラのリスナー
 		// シャッターを切った時のコールバック
 		public void OnPictureTaken(byte[] data, AndroidCamera camera) {
 			try {
@@ -176,27 +278,37 @@ namespace Camera {
 					SaveDir.Mkdir();
 				}
 
-				// 保存ディレクトリに入ってるファイル数をカウント
-				var Files = SaveDir.List();
-				int count = 0;
-				foreach(var tmp in Files){
-					count++;
-				}
 
-				Matrix matrix = new Matrix();	// 回転用の行列
-				matrix.SetRotate(90 - DetectScreenOrientation());
-				Bitmap original = BitmapFactory.DecodeByteArray(data, 0, data.Length);
-				Bitmap rotated = Bitmap.CreateBitmap(original, 0, 0, original.Width, original.Height, matrix, true);
-
-				var FileName = new Java.IO.File(SaveDir, "DCIM_" + (count + 1) + ".jpg");
-
-				// 非同期で画面の回転処理とアルバムへの登録を行う
+				// 非同期で画像の回転・保存・アルバムへの登録
 				Task.Run(async () => {
+
+					// 保存ディレクトリに入ってるファイル数をカウント
+					var Files = SaveDir.List();
+					int count = 0;
+					foreach (var tmp in Files) {
+						count++;
+					}
+
+					Matrix matrix = new Matrix();   // 回転用の行列
+					matrix.SetRotate(90 - DetectScreenOrientation());
+					Bitmap original = BitmapFactory.DecodeByteArray(data, 0, data.Length);
+					Bitmap rotated = Bitmap.CreateBitmap(original, 0, 0, original.Width, original.Height, matrix, true);
+
+					var FileName = new Java.IO.File(SaveDir, "DCIM_" + (count + 1) + ".jpg");
+
 
 					// ファイルをストレージに保存
 					FileStream stream = new FileStream(FileName.ToString(), FileMode.CreateNew);
 					await rotated.CompressAsync(Bitmap.CompressFormat.Jpeg, 90, stream);
 					stream.Close();
+
+					Android.Media.ExifInterface Exif = new ExifInterface(FileName.ToString());
+					Exif.SetAttribute(ExifInterface.TagGpsLatitude, Latitude);
+					Exif.SetAttribute(ExifInterface.TagGpsLongitude, Longitude);
+					Exif.SetAttribute(ExifInterface.TagGpsLatitudeRef, "N");
+					Exif.SetAttribute(ExifInterface.TagGpsLongitudeRef, "E");
+					Exif.SaveAttributes();
+
 
 					// 保存したファイルをアルバムに登録
 					string[] FilePath = { Environment.GetExternalStoragePublicDirectory(Environment.DirectoryDcim) + "/Camera/" + "DCIM_" + (count + 1) + ".jpg" };
@@ -222,6 +334,11 @@ namespace Camera {
 			if (isTakeEnabled)
 				m_Camera.TakePicture(null, null, this);
 		}
+
+		public void OnAutoFocusMoving(bool success, AndroidCamera camea){
+			
+		}
+		#endregion
 
 		// 画面の向きに合わせてSurfaceViewの向きを変更
 		public bool SetScreenOrientation(){
@@ -277,7 +394,6 @@ namespace Camera {
 			return ReturnValue;
 
 		}
-
 
 		// 画面のサイズを取得する
 		public void GetDisplaySize(){
